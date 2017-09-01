7d1a1113fb31a7068e2c9df2b8eee150752c5284
Alex <giwan.kwon@gmail.com>
2017-09-01 04:05:05 -0700

    facebook login button

--- Tumbleweed/app/screens/Onboard.js
+++ Tumbleweed/app/screens/Onboard.js
@@ -8,6 +8,33 @@ import { width, height, totalSize } from 'react-native-dimension';
 import renderIf from '../config/renderIf';
 import Modal from 'react-native-modal';
 
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+} = FBSDK;
+      
+var Login = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
+          }
+          onLogoutFinished={() => alert("User logged out")}/>
+      </View>
+    );
+  }
+});
 
 
 class Onboard extends Component {
@@ -309,13 +336,15 @@ class Onboard extends Component {
                   source={require('../assets/onboarding/ReserveButton.png')}
                 />
               </TouchableOpacity>
-        */}
-        <View style={styles.slide1}>
-          <Image
+
+              <Image
           style={{height:730, width:375}}
           source={require('../assets/onboarding/BlurOverlay.png')}
           />
+        */}
+        <View style={styles.slide1}>
           
+          <Login />
 
 
         </View>
